dbc6bc36382e20141bc52bacf7e0f4e070bfc3b0
MilenaPL <92209272+MilenaPL@users.noreply.github.com>
2022-02-02 20:37:03 +0100

    Update Day_2.py

--- Day_2.py
+++ Day_2.py
@@ -1,4 +1,4 @@
-#addiction number in string
+# addiction number in string
 
 two_digit_number = input("Type a two digit number: ")
 
@@ -8,10 +8,7 @@ second = two_digit_number[1]
 print(int(first)+int(second))
 
 
-
-
-
-#BMI calculator
+# BMI calculator
 
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
@@ -23,10 +20,7 @@ print("Your BMI is:")
 print(weight_num/height_num**2)
 
 
-
-
-
-#Life in days if you will live 90 years
+# Life in days if you will live 90 years
 
 age = input("What is your current age?")
 
@@ -39,21 +33,19 @@ months = (years * 12)
 print(f"You have {days} days, or {weeks} weeks, or {months} months left.")
 
 
-
-
-
-#Tip calculator - 2 decimal places
+# Tip calculator - 2 decimal places
 
 print("Welcome to tip calculator!")
 bill = input("What was the total bill?")
-amount_of_tip = input("How much tip would you like to give? 10%, 12% or 15% ?")
+amount_of_tip = input(
+    "How much tip would you like to give? 10, 12 or 15% ? - enter a number")
 how_many_people = input("How many people to split the bill?")
 
-total_bill = int(bill)
+total_bill = float(bill)
 percent_num = int(amount_of_tip)
 tip = (percent_num/100) * total_bill
 people = int(how_many_people)
 each_person = (total_bill + tip) / people
 each_person2 = round(each_person, 2)
 
-print(f"Each person should pay $ {each_person2}") 
+print(f"Each person should pay $ {each_person2}")
